b681835f33e7b412dcfd4e27a65c4f2554ce245e
Jean-D-alves <jenavinicius64@gmail.com>
2026-01-13 15:52:16 -0300

    alert element

--- public/js/login.js
+++ public/js/login.js
@@ -17,14 +17,42 @@ form.addEventListener("submit", async (e) => {
       }),
     });
     const data = await response.json();
-    console.log(data)
+    if (data) {
+      ElementAlert(
+        data.statusText,
+        data.status === 200 ? "positive" : "negativo"
+      );
+    }
     const token = SaveToken(data.data.token);
     return token;
   } catch (error) {
     console.log(error);
   }
 });
+function ElementAlert(message, status) {
+  const main = document.querySelector("main");
 
+  const div = document.createElement("div");
+  div.classList.add("AlertElement");
+
+  const button = document.createElement("button");
+  button.textContent = "x";
+  button.classList.add("ButtonAlert");
+  button.onclick = ()=>{
+    main.removeChild(div)
+  };
+  const text = document.createElement("p");
+  text.textContent = message;
+
+  if (status === "negativo") {
+    div.classList.add("negativo");
+    button.classList.add("negativo");
+  }
+
+  div.appendChild(text);
+  div.appendChild(button);
+  main.appendChild(div);
+}
 function SaveToken(token) {
   const save = localStorage.setItem("token", token);
   return save;
