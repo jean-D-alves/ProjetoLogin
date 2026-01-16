018a9be6b650ceae5648f4883a000df199f30d4c
Jean-D-alves <jenavinicius64@gmail.com>
2026-01-16 10:17:01 -0300

    mudança de banco

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,23 +1,15 @@
-import "dotenv/config";
+import "dotenv/config"
 
 import pkg from "pg";
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString:process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-console.log(`  host:${process.env.DB_HOST},
-  user: ${process.env.DB_USER},
-  password: ${process.env.DB_PASSWORD},
-  database: ${process.env.DB_NAME},
-  port: ${process.env.DB_PORT},`);
+
 
 export default pool;
